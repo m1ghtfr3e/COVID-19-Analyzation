f7da97c233de9516432dfebda554c3290bb7c03d
m1ghtfr3e <48571260+m1ghtfr3e@users.noreply.github.com>
2020-03-19 17:03:58 +0000

    MODULE
    should update dataframe when new file comes to working directory,
    not really working right now

---
+++ updatejson.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 16:26:49 2020
+
+@author: m1ghtfr3e
+"""
+
+import pandas as pd
+import glob
+
+def updateFile():
+    path = "/home/n0way/PythonEnv/Python/Coivd-19/progr"
+    allfiles = glob.glob(path + "/*.json")
+
+    Data = []
+    for file in allfiles:
+        df = pd.read_json(file)
+        Data.append(df)
+
+        alldata = pd.concat(Data)
+        return alldata
